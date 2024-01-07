4ad7e2c7e0db2b97aaa7b598c1e0ce26a24540e1
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2024-01-07 10:59:36 +0100

    feat: creating home.module

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,8 @@ export default function RootLayout({
     <html lang="en">
       {/*el layout envuelve la aplicacion y las rutas que se van a renderizar, es decir, el page.tsx*/}
       <body>
-        <h1>Titulo</h1>
         {/* este children seria el page.tsx */}
-        <div style={{ border: '1px solid #000' }}>{children}</div>
+        {children}
       </body>
     </html>
   );
