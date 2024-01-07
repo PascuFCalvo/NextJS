8bfd3e5a077761f2e329d7f5869db3c9d981a729
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2024-01-07 10:53:39 +0100

    feat: adding global styles to the app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import './ui/global.css';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/*el layout envuelve la aplicacion y las rutas que se van a renderizar, es decir, el page.tsx*/}
+      <body>
+        <h1>Titulo</h1>
+        {/* este children seria el page.tsx */}
+        <div style={{ border: '1px solid #000' }}>{children}</div>
+      </body>
     </html>
   );
 }
