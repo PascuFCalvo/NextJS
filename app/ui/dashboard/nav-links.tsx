156e8a713161c84a266e751516a60598c79a264a
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2024-01-07 12:20:10 +0100

    feat: adding conditional styles using usePathname and using use Client directive

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,8 +1,12 @@
+//hacemos que este componente funcione del lado del cliente
+'use client';
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -17,19 +21,26 @@ const links = [
 ];
 
 export default function NavLinks() {
+  //vamos a mejorar la experienca de usuario, sabiendo en que path esta en cada momento, para eso vamos a usar el hook usePathname de nextJS.
+  const pathname = usePathname();
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          // vamos a cambiar el anchor por el componente link para crear la navecagion de una SPA mucho mas rapida y que no cargue toda la pagina
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={`flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3
+
+
+            ${pathname === link.href ? 'bg-sky-100 text-blue-600' : ''}`}
+            // vamos a estilar el link que esta activo
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
