b69502384699f29ae2a4d13a562ea9a6ded8c55f
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2024-01-08 11:09:22 +0100

    feat: sinc query params to default search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -15,9 +15,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handlerSearch = (term: string) => {
     const params = new URLSearchParams(searchParams);
     if (term) {
-      params.set('search', term);
+      params.set('query', term);
     } else {
-      params.delete('search');
+      params.delete('query');
     }
 
     replace(`${pathnmae}?${params.toString()}`);
@@ -31,6 +31,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => handlerSearch(e.target.value)}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
