5baed28a6774d7b6f2fe2b557a677584d96f1231
PascuFCalvo <pascual.fernandez.calvo@gmail.com>
2024-01-08 09:51:59 +0100

    feat: create loading.tsx to show something while fetching data

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loadin() {
+  return <div>Cargando...</div>;
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,9 @@
 
 //nosotros nos vamos a importar el fecth que esta en lib
 
-import { fetchRevenue } from '../lib/data';
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '../lib/data';
+import { Card } from '../ui/dashboard/cards';
+import LatestInvoices from '../ui/dashboard/latest-invoices';
 import RevenueChart from '../ui/dashboard/revenue-chart';
 import { lusitana } from '../ui/fonts';
 
@@ -13,18 +15,28 @@ export default async function DashboardPage() {
   // const res = await fetch("http://api.random.com")
   // const json = await res.json()
   // console.log(json)
-
+  const latestInvoices = await fetchLatestInvoices();
   const revenue = await fetchRevenue();
-  console.log(revenue);
+  const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
 
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
-      <div className="gird gap-6 sm:grid-cols-2 lg:grid-cols-4"></div>
+      <div className="gird gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
